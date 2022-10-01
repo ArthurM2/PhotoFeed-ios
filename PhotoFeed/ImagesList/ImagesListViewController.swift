b02e8c72320788713093d4b42e247d4ff5776392
a.malginov <a.malginov@rambler-co.ru>
2022-10-01 12:25:45 +0500

    fix

--- PhotoFeed/ImagesList/ImagesListViewController.swift
+++ PhotoFeed/ImagesList/ImagesListViewController.swift
@@ -31,7 +31,7 @@ extension ImagesListViewController {
         cell.dateLabel.text = dateFormatter.string(from: Date())
 
         let isLiked = indexPath.row % 2 == 0
-        let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
+        let likeImage = isLiked ? UIImage(named: "button_like_on") : UIImage(named: "button_like_off")
         cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
