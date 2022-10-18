0d66e6879738d83d7de5d9b5a9ec0a4b1d945962
a.malginov <a.malginov@rambler-co.ru>
2022-10-18 15:49:42 +0500

    added rescale func

--- PhotoFeed/SingleImageView/SingleImageViewController.swift
+++ PhotoFeed/SingleImageView/SingleImageViewController.swift
@@ -5,22 +5,40 @@ final class SingleImageViewController: UIViewController {
         didSet {
             guard isViewLoaded else { return }
             imageView.image = image
+            rescaleScrollViewForPerfectView(image: image)
         }
     }
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var scrollView: UIScrollView!
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
-        
+        rescaleScrollViewForPerfectView(image: image)
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
     }
 
-    @IBAction private func didTapBackButton() {
-        dismiss(animated: true, completion: nil)
+    private func rescaleScrollViewForPerfectView(image: UIImage) {
+        let containerSize = view.bounds.size
+        let imageSize = image.size
+        let hScale = containerSize.width / imageSize.width
+        let vScale = containerSize.height / imageSize.height
+        let scale = max(hScale, vScale)
+        let expectedContentSize = CGSize(
+            width: imageSize.width * scale,
+            height: imageSize.height * scale
+        )
+        UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseIn) {
+            self.scrollView.setZoomScale(scale, animated: false)
+            let x = (expectedContentSize.width - containerSize.width) / 2
+            let y = (expectedContentSize.height - containerSize.height) / 2
+            self.scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+        } completion: { _ in }
     }
 }
 
